84c586385240721c658453c3a0272ff65a1a3f6e
Manmay <manmay.singh@gmail.com>
2023-03-06 02:50:53 +0530

    custom side nav bar created, each dropdown is seperated from other, each dropdown has it's own highlight when selected

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'testing-angular-tailwind';
+  toggle = (event: MouseEvent) => {
+    let parent = (event.target as Element);
+    let dropdown_id = 'dropdown-' + parent.id;
+
+    let menu = document.getElementsByClassName('menu')
+    for (let i = 0; i < menu.length; i++){
+      let k = menu[i].firstElementChild
+      if (k != null && !(k.id ===parent.id)){
+        k.classList.remove('font-semibold')
+        k.classList.remove('bg-blue-200')
+      }
+    }
+    
+    
+    let dropdown_menu = document.getElementsByClassName('dropdown-menu');
+    for (let i = 0; i < dropdown_menu.length; i++){
+      if (!dropdown_menu[i].classList.contains('hidden') && !(dropdown_menu[i].id ===dropdown_id)){
+        dropdown_menu[i].classList.add('hidden')
+      }
+    }
+
+
+    let dropdown_ele = document.getElementById(dropdown_id);
+    dropdown_ele?.classList.toggle('hidden');
+    parent.classList.toggle('font-semibold');
+    parent.classList.toggle('bg-blue-200')
+  };
 }
